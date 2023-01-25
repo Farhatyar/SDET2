9a82674a60b658b236b45d9136a415e719e6edf8
omidsalaah <aoblfazelsalaah@gmail.com>
2023-01-24 19:17:00 -0500

    v7

--- src/class12/StringDemo10.java
+++ src/class12/StringDemo10.java
@@ -11,7 +11,6 @@ public class StringDemo10 {
 
         for (int i = 0; i < str.length(); i++) {
             System.out.println(str.charAt(i)+" has the index of: "+i);
-            System.out.println();
         }
     }
 }
